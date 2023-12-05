3f20e46ffe9e93436fd48325ac72abaa5cb2ce24
roebuck45 <j_seirer@mail.fhsu.edu>
2023-12-05 11:58:50 -0600

    more stuff

--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    <li><a href="{% url 'detail' question.id %}">{{ question.question_text }}</a></li>
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
